4f28bf5091cb85f4ae9432728e3ccb245b5cfd9a
AlexBelov0375 <43350621+AlexBelov0375@users.noreply.github.com>
2022-11-08 23:47:19 +0300

    Update Meteo.ino

--- Meteo.ino
+++ Meteo.ino
@@ -1,11 +1,27 @@
-#include <ESP8266WiFi.h> //Подключение библиотеки для работы с сетью
-#include <DallasTemperature.h> //Подключение библиотеки для работы с датчиком DS18B20
 
-char ssid[] = "Home"; // Идентификатор вашей сети wifi
-char password[] = "Password"; //пароль вашей сети wifi
+// Demonstrate the use of WiFi.shutdown() and WiFi.resumeFromShutdown()
+// Released to public domain
+
+// Current on WEMOS D1 mini (including: LDO, usbserial chip):
+// ~85mA during normal operations
+// ~30mA during wifi shutdown
+//  ~5mA during deepsleep
+
+#ifndef STASSID
+#define STASSID "Home"
+#define STAPSK ""
+#endif
+
+#ifndef RTC_USER_DATA_SLOT_WIFI_STATE
+#define RTC_USER_DATA_SLOT_WIFI_STATE 33u
+#endif
 
 #define ONE_WIRE_BUS 0 //D3 пин на плате (Линия данных для DS18B20)
 #define DS18B20_POWER 5 //D1 пин на плате (Питание для DS18B20 для ручного управляения питанием датчика)
+
+#include <ESP8266WiFi.h>
+#include <include/WiFiState.h> // WiFiState structure details
+#include <DallasTemperature.h> //Подключение библиотеки для работы с датчиком DS18B20
 
 // создаем экземпляр класса oneWire; с его помощью 
 // можно коммуницировать с любыми девайсами, работающими 
@@ -13,28 +29,22 @@ char password[] = "Password"; //пароль вашей сети wifi
 // от компании Maxim/Dallas:
 OneWire oneWire(ONE_WIRE_BUS);
 
-DallasTemperature DS18B20(&oneWire);  
-WiFiClient client;
-IPAddress ip(192, 168, 1, 8); //Настройка статического IP адреса в локальной сети
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 255, 0);
+DallasTemperature DS18B20(&oneWire); 
 
-void toWifi() {
-  WiFi.begin(ssid, password); //Подключение к роутеру
-  WiFi.config(ip, gateway, subnet); //Задание настроек статического подключения
-}
+WiFiState state;
+WiFiClient client;
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+unsigned long duration; //Время подключения
 
 float getTemperature() {
   float tempC;
   do {
     DS18B20.requestTemperatures(); //Запрос измерения температуры
-    delay(100); 
-    //Задерка для преобразования
-    //9-бит 93,75мс
-    //10-бит 185,5мс
-    //11-бит 375мс
-    //12-бит 750мс
     tempC = DS18B20.getTempCByIndex(0);
+    Serial.printf("Temp: %f", tempC);
+    Serial.println();    
   } while (tempC == 85.0 || tempC == (-127.0)); //В случае некорректных показаний
   return tempC;
 }
@@ -47,30 +57,79 @@ void data(float tempC) {
       buf = buf + "#";
       for (uint8_t i = 0; i < 8; i++) {if (tempDeviceAddress[i] < 16) buf = buf + "0"; buf = buf + String(tempDeviceAddress[i], HEX);} //Адрес датчика в буфер
       buf = buf + "#" + String(tempC)+ "\r\n"; //и температура
+      buf = buf + "#Duration" + "#" + String(duration * 0.001)+ "\r\n"; //и задержка
       buf = buf + "##\r\n"; // закрываем пакет
-      while (WiFi.status() != WL_CONNECTED) { //До этого датчик пытался подключится и только тут делаем проверку для оптимизации времени
-         delay(30);
-      }
       client.connect("185.245.187.136", 8283); //Подключаемся
       client.print(buf); // И отправляем данные
       client.stop();
       client.flush();
 }
+
 void setup() {
+  Serial.begin(115200);
+  //Serial.setDebugOutput(true);  // If you need debug output
+  Serial.println();
+  Serial.println("Trying to resume WiFi connection...");
+
+  // May be necessary after deepSleep. Otherwise you may get "error: pll_cal exceeds 2ms!!!" when trying to connect
+  delay(1);
+
+  // ---
+  // Here you can do whatever you need to do that doesn't need a WiFi connection.
+  // ---
   pinMode(DS18B20_POWER, OUTPUT); //Задаём пин для питания DS18B20
   digitalWrite(DS18B20_POWER, HIGH); //Включаем его питание
-  toWifi();
   DS18B20.begin(); // по умолчанию разрешение датчика – 9-битное;
                    // если у вас какие-то проблемы, его имеет смысл
                    // поднять до 12 бит; если увеличить задержку, 
                    // это даст датчику больше времени на обработку
                    // температурных данных
-  //sensor.setResolution(12);
   float tempC = getTemperature();
+  
+  ESP.rtcUserMemoryRead(RTC_USER_DATA_SLOT_WIFI_STATE, reinterpret_cast<uint32_t *>(&state), sizeof(state));
+  unsigned long start = millis();
+
+  if (!WiFi.resumeFromShutdown(state)
+      || (WiFi.waitForConnectResult(10000) != WL_CONNECTED)) {
+    Serial.println("Cannot resume WiFi connection, connecting via begin...");
+    WiFi.persistent(false);
+
+    if (!WiFi.mode(WIFI_STA)
+        || !WiFi.begin(ssid, password)
+        || (WiFi.waitForConnectResult(10000) != WL_CONNECTED)) {
+      WiFi.mode(WIFI_OFF);
+      Serial.println("Cannot connect!");
+      Serial.flush();
+      ESP.deepSleep(10e6, RF_DISABLED);
+      return;
+    }
+  }
+
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+
+  duration = millis() - start;
+  Serial.printf("Duration: %f", duration * 0.001);
+  Serial.println();
+
+  // ---
+  // Here you can do whatever you need to do that needs a WiFi connection.
+  // ---
   data(tempC);
+
+  WiFi.shutdown(state);
+  ESP.rtcUserMemoryWrite(RTC_USER_DATA_SLOT_WIFI_STATE, reinterpret_cast<uint32_t *>(&state), sizeof(state));
+
+  // ---
+  // Here you can do whatever you need to do that doesn't need a WiFi connection anymore.
+  // ---
   digitalWrite(DS18B20_POWER, LOW); //Отключаем питание датчика перед сном
-  ESP.deepSleep(315e6);//Сон на 5 минут (в вашем случае может быть другая погрешность таймера)
+  
+  Serial.println("Done.");
+  Serial.flush();
+  ESP.deepSleep(315e6, RF_DISABLED);
 }
 
 void loop() {
+  // Nothing to do here.
 }
